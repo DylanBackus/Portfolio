0a5dbd96b8aa9982fed134779bee463a4842eb21
Dylan Backus <dylanbackus024@gmail.com>
2024-09-06 08:54:58 +0200

    Metaballs Script optimized

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 var canvas = document.createElement("canvas");
-var width = canvas.width = window.innerWidth * 0.60;
-var height = canvas.height = window.innerHeight * 0.60; 
+var width = (canvas.width = window.innerWidth * 0.6);
+var height = (canvas.height = window.innerHeight * 0.6); //container height en width
 document.body.appendChild(canvas);
-var gl = canvas.getContext('webgl');
+var gl = canvas.getContext("webgl");
 
 var mouse = { x: 0, y: 0 };
 
-var numMetaballs = 40;  
+var numMetaballs = 40; // aantal
 var metaballs = [];
 
 for (var i = 0; i < numMetaballs; i++) {
-    var radius = Math.random() * 80; 
-    metaballs.push({
-        x: Math.random() * (width - 10 * radius) + radius,
-        y: Math.random() * (height - 10 * radius) + radius,
-        vx: (Math.random() - 0.002) * 1.2,
-        vy: (Math.random() - 0.002) * 1.2, 
-        r: radius * .9
-    });
+  var radius = Math.random() * 80;
+  metaballs.push({
+    x: Math.random() * (width - 10 * radius) + radius,
+    y: Math.random() * (height - 10 * radius) + radius,
+    vx: (Math.random() - 0.002) * 1.2,
+    vy: (Math.random() - 0.002) * 1.2, //speed
+    r: radius * 0.9,
+  });
 }
 
 var vertexShaderSrc = `
@@ -28,20 +28,29 @@ var vertexShaderSrc = `
     }
 `;
 
-var fragmentShaderSrc = `
+var fragmentShaderSrc =
+  `
     precision highp float;
 
-    const float WIDTH = ` + (width >> 0) + `.0;
-    const float HEIGHT = ` + (height >> 0) + `.0;
+    const float WIDTH = ` +
+  (width >> 0) +
+  `.0;
+    const float HEIGHT = ` +
+  (height >> 0) +
+  `.0;
 
-    uniform vec3 metaballs[` + numMetaballs + `];
+    uniform vec3 metaballs[` +
+  numMetaballs +
+  `];
 
     void main(){
         float x = gl_FragCoord.x;
         float y = gl_FragCoord.y;
 
         float sum = 0.0;
-        for (int i = 0; i < ` + numMetaballs + `; i++) {
+        for (int i = 0; i < ` +
+  numMetaballs +
+  `; i++) {
             vec3 metaball = metaballs[i];
             float dx = metaball.x - x;
             float dy = metaball.y - y;
@@ -68,83 +77,74 @@ gl.attachShader(program, fragmentShader);
 gl.linkProgram(program);
 gl.useProgram(program);
 
-var vertexData = new Float32Array([
-    -1.0, 1.0,
-    -1.0, -1.0,
-    1.0, 1.0,
-    1.0, -1.0,
-]);
+var vertexData = new Float32Array([-1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0]);
 var vertexDataBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
 
-var positionHandle = getAttribLocation(program, 'position');
+var positionHandle = getAttribLocation(program, "position");
 gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(positionHandle,
-    2,
-    gl.FLOAT,
-    gl.FALSE,
-    2 * 4,
-    0
-);
+gl.vertexAttribPointer(positionHandle, 2, gl.FLOAT, gl.FALSE, 2 * 4, 0);
 
-var metaballsHandle = getUniformLocation(program, 'metaballs');
+var metaballsHandle = getUniformLocation(program, "metaballs");
 
 loop();
 function loop() {
-    for (var i = 0; i < numMetaballs; i++) {
-        var metaball = metaballs[i];
-        metaball.x += metaball.vx;
-        metaball.y += metaball.vy;
+  for (var i = 0; i < numMetaballs; i++) {
+    var metaball = metaballs[i];
+    metaball.x += metaball.vx;
+    metaball.y += metaball.vy;
 
-        if (metaball.x < metaball.r || metaball.x > width - metaball.r) metaball.vx *= -1;
-        if (metaball.y < metaball.r || metaball.y > height - metaball.r) metaball.vy *= -1;
-    }
+    if (metaball.x < metaball.r || metaball.x > width - metaball.r)
+      metaball.vx *= -1;
+    if (metaball.y < metaball.r || metaball.y > height - metaball.r)
+      metaball.vy *= -1;
+  }
 
-    var dataToSendToGPU = new Float32Array(3 * numMetaballs);
-    for (var i = 0; i < numMetaballs; i++) {
-        var baseIndex = 3 * i;
-        var mb = metaballs[i];
-        dataToSendToGPU[baseIndex + 0] = mb.x;
-        dataToSendToGPU[baseIndex + 1] = mb.y;
-        dataToSendToGPU[baseIndex + 2] = mb.r;
-    }
-    gl.uniform3fv(metaballsHandle, dataToSendToGPU);
+  var dataToSendToGPU = new Float32Array(3 * numMetaballs);
+  for (var i = 0; i < numMetaballs; i++) {
+    var baseIndex = 3 * i;
+    var mb = metaballs[i];
+    dataToSendToGPU[baseIndex + 0] = mb.x;
+    dataToSendToGPU[baseIndex + 1] = mb.y;
+    dataToSendToGPU[baseIndex + 2] = mb.r;
+  }
+  gl.uniform3fv(metaballsHandle, dataToSendToGPU);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-    requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 
 function compileShader(shaderSource, shaderType) {
-    var shader = gl.createShader(shaderType);
-    gl.shaderSource(shader, shaderSource);
-    gl.compileShader(shader);
+  var shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
 
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
-    }
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
+  }
 
-    return shader;
+  return shader;
 }
 
 function getUniformLocation(program, name) {
-    var uniformLocation = gl.getUniformLocation(program, name);
-    if (uniformLocation === -1) {
-        throw 'Can not find uniform ' + name + '.';
-    }
-    return uniformLocation;
+  var uniformLocation = gl.getUniformLocation(program, name);
+  if (uniformLocation === -1) {
+    throw "Can not find uniform " + name + ".";
+  }
+  return uniformLocation;
 }
 
 function getAttribLocation(program, name) {
-    var attributeLocation = gl.getAttribLocation(program, name);
-    if (attributeLocation === -1) {
-        throw 'Can not find attribute ' + name + '.';
-    }
-    return attributeLocation;
+  var attributeLocation = gl.getAttribLocation(program, name);
+  if (attributeLocation === -1) {
+    throw "Can not find attribute " + name + ".";
+  }
+  return attributeLocation;
 }
 
 canvas.onmousemove = function (e) {
-    mouse.x = e.clientX;
-    mouse.y = e.clientY;
-}
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+};
