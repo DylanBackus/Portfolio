6b134e465c9c16d136964014b75f4b94e4769937
DylanBackus <1202532@student.roc-nijmegen.nl>
2024-09-08 20:14:14 +0200

    Namen overzichtelijker gemaakt

--- js/about-scroll.js
+++ js/about-scroll.js
@@ -5,17 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
   var observer = new IntersectionObserver(function (entries) {
     entries.forEach(function (entry) {
       if (entry.isIntersecting) {
-        // Element is zichtbaar. fade-in toepassen
+        // Element is zichtbaar dus fade-in toepassen
         aboutSection.style.transition = "opacity 1.5s ease-in-out";
         aboutSection.style.opacity = 1;
       } else {
-        // Element is niet zichtbaar, fade-out toepassen
+        // Element is niet zichtbaar dus fade-out toepassen
         aboutSection.style.transition = "opacity 1.5s ease-in-out";
         aboutSection.style.opacity = 0;
       }
     });
   }, {
-    threshold: 0.6 //fade-in gebeurt wanneer 60% van het element zichtbaar is
+    threshold: 0.6 //fade in gebeurt wanneer 60% van het element zichtbaar is
   });
 
   observer.observe(aboutSection);
--- js/cursor-effect.js
+++ js/cursor-effect.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cursor = document.querySelector('.cursor');
-  
-    document.addEventListener('mousemove', (e) => {
-      // Gebruik clientX en clientY voor correcte positie, ongeacht scrollen
-      cursor.style.left = `${e.clientX}px`;
-      cursor.style.top = `${e.clientY}px`;
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const cursor = document.querySelector(".cursor");
+
+  document.addEventListener("mousemove", (e) => {
+    // Gebruik clientx en clienty voor correcte positie omdat page niet werkt met een langere website zegmaar
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
   });
-  
+});
