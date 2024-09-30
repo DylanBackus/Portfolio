493fae5386811baccfc539d166b53bc3ae06c08d
Dylan Backus <dylanbackus024@gmail.com>
2024-09-30 11:58:10 +0200

    images added

--- js/about-scroll.js
+++ js/about-scroll.js
@@ -1,21 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   var aboutSection = document.querySelector(".about-me-second-text-container");
 
-  // maken van de IntersectionObserver
   var observer = new IntersectionObserver(function (entries) {
     entries.forEach(function (entry) {
       if (entry.isIntersecting) {
-        // Element is zichtbaar dus fade-in toepassen
         aboutSection.style.transition = "opacity 1.5s ease-in-out";
         aboutSection.style.opacity = 1;
       } else {
-        // Element is niet zichtbaar dus fade-out toepassen
         aboutSection.style.transition = "opacity 1.5s ease-in-out";
         aboutSection.style.opacity = 0;
       }
     });
   }, {
-    threshold: 0.6 //fade in gebeurt wanneer 60% van het element zichtbaar is
+    threshold: 0.6 
   });
 
   observer.observe(aboutSection);
